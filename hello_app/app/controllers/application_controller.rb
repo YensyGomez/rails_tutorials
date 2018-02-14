c16639c18189ed36baad164829304366981336a2
YensyGomez <yensy@sirius.utp.edu.co>
2018-02-14 14:46:06 -0500

    chapter1 homework2 MVC controller/routes

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+    render html: "¡hola Mundo!"
+  end
+  def goodbye
+    render html: "¡goodbye, world!"
+  end
 end
